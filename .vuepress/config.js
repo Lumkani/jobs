1735636c2d16273591fd44f00c0e302e3f33d572
shailen <shailen@lumkani.com>
2019-09-15 20:36:48 +0200

    Removed components section

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,10 +4,6 @@ module.exports = {
     logo: 'https://avatars3.githubusercontent.com/u/18303687?s=400&u=cdffc0d3253e9247ed24d5a3e70a712cef71d133&v=4',
     sidebar: [
       {
-        title: 'Components',
-        path: '/components/'
-      },
-      {
         title: 'RFCs',
         path: '/rfcs/'
       }
