0c9b74572108d4a3c04209d4c0fa06abc3cc33c6
Shailen Naidoo <shailen@lumkani.com>
2019-10-21 11:17:26 +0200

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,6 @@ module.exports = {
   themeConfig: {
     logo: 'https://avatars3.githubusercontent.com/u/18303687?s=400&u=cdffc0d3253e9247ed24d5a3e70a712cef71d133&v=4',
     sidebar: false,
-    search: false,
+    search: true,
   },
 };
